dcbf00cf0e052c7618572a9d603339ce5dfde1d7
DmitryTrollgiev <58760467+DmitryTrollgiev@users.noreply.github.com>
2019-12-11 19:40:25 +0300

    Взяли пример изначальный

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,20 @@
-#Бот
-#https://github.com/eternnoir/pyTelegramBotAPI
-#
-#Сайт
-#
-#https://kino-galaktika.ru/?date=2019/12/11&city=cheboksari
+import telebot
+
+API_TOKEN = ""
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+@bot.message_handler(commands=['kot'])
+def send_check(message):
+    bot.reply_to(message, "РАБОТАЕТ")
+
+bot.polling()
