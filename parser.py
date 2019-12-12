2ef0de08a757245ba94eea6fc44a288304d89b77
DmitryTrollgiev <58760467+DmitryTrollgiev@users.noreply.github.com>
2019-12-12 16:14:29 +0300

    Реформатирование исходного кода

--- parser.py
+++ parser.py
@@ -1,18 +1,18 @@
 import json
+
 import requests
 from bs4 import BeautifulSoup
 
+
 def parser_outer(bot, message, jsn, film_name):
     d = json.loads(jsn)
-    out_str = "<b>"+film_name+"</b>"
+    out_str = "<b>" + film_name + "</b>"
     if d["exception"] == False:
         for timetable in d["info"]:
             content = d["info"][timetable]
-            out_str += "\n"+timetable+" - "+content["price"]+" в "+content["type"]
-            
+            out_str += "\n" + timetable + " - " + content["price"] + " в " + content["type"]
+
         bot.reply_to(message, out_str, parse_mode='HTML')
-    else:
-        bot.reply_to(message, "<b>"+film_name+"</b>\nОшибка парсинга", parse_mode='HTML')
 
 
 def get_site(url):
@@ -22,26 +22,27 @@ def get_site(url):
     r = requests.get(url).text
     return r
 
+
 def locale_html_parser(url):
     """
     Функция генерации словаря на основе расписания + парсинг сайта
     """
     locale_dict = {}
-    #Парсим ссылку с фильмом
+    # Парсим ссылку с фильмом
     locale_soup = BeautifulSoup(get_site(url), "lxml")
     all_timetables = locale_soup.find("div", class_="sc-bdVaJa sc-bwzfXH iNSiJW schedule")
-                
-    #Время всех сеансов текущего фильма
+
+    # Время всех сеансов текущего фильма
     time_list = []
     for time in all_timetables.find_all("div", class_="sc-bdVaJa sc-bwzfXH buSbSY"):
         time_list.append(time.text)
-                
-    #Цена в руб для всех сеансов текущего фильма
+
+    # Цена в руб для всех сеансов текущего фильма
     price_list = []
     for price in all_timetables.find_all("div", class_="sc-bdVaJa sc-bwzfXH sw9zb-0 dDMLGB price"):
         price_list.append(price.text)
-                
-    #Тип фильма (3D/2D) для всех сенсов текущего фильма
+
+    # Тип фильма (3D/2D) для всех сенсов текущего фильма
     film_type_list = []
     for film_type in all_timetables.find_all("div", class_="sc-bdVaJa sc-bwzfXH gYCEQ sw9zb-1 jXcRgy formats"):
         film_str = film_type.text
@@ -53,7 +54,7 @@ def locale_html_parser(url):
             film_type_list.append("???")
 
     if len(time_list) == len(price_list) and len(film_type_list) == len(time_list):
-        #Заносим в словарь
+        # Заносим в словарь
         for i in range(len(time_list)):
             locale_dict[time_list[i]] = {"price": price_list[i], "type": film_type_list[i]}
         if locale_dict == {}:
@@ -61,7 +62,8 @@ def locale_html_parser(url):
         return {"info": locale_dict, "exception": False}
     else:
         return {"info": {}, "exception": True}
-    
+
+
 def html_parser(bot, message, date, city):
     """
     Основной генератор-парсер словаря
@@ -69,30 +71,31 @@ def html_parser(bot, message, date, city):
 
     URL = "https://kino-galaktika.ru"
 
-    r = get_site(URL+"/?date="+date+"&city="+city)
+    r = get_site(URL + "/?date=" + date + "&city=" + city)
     soup = BeautifulSoup(r, "lxml")
-    
-    #Словарь для хранения фильма и ссылки на него
+
+    # Словарь для хранения фильма и ссылки на него
     films_urls = {}
-    #Формируем Список всех фильмов
+    # Формируем Список всех фильмов
     for link in soup.find_all("h2", class_="sc-1h5tg87-0 duYTXo title"):
-        #Находим ссылку на фильм
+        # Находим ссылку на фильм
         for a in link.find_all('a', href=True):
-            if "&city="+city in a['href']:
-                
+            if "&city=" + city in a['href']:
+
                 film_name = link.text
                 if film_name not in films_urls:
-                    films_urls[film_name] = URL+a['href']
-                
-    #Для каждого фильма получаем расписание в отельной функции    
+                    films_urls[film_name] = URL + a['href']
+
+    # Для каждого фильма получаем расписание в отельной функции
     for film in films_urls:
         result = locale_html_parser(films_urls[film])
         json_result = to_json(result)
         parser_outer(bot, message, json_result, film)
 
+
 def to_json(d):
     """
     Перевод в JSON
     """
-    result_json = json.dumps(d,ensure_ascii=False)
+    result_json = json.dumps(d, ensure_ascii=False)
     return result_json
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,46 +1,57 @@
 import telebot
+
 import parser
 
-API_TOKEN = "908179410:AAGTLbJbC1n6AnLbqq66cfLlQV9YdC48llU"
+API_TOKEN = "TOKEN"
 
 bot = telebot.TeleBot(API_TOKEN)
 users_d = {}
 
-#Обработка старта программы
+
+# Обработка старта программы
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
     if message.from_user.id not in users_d:
-        users_d[message.from_user.id] = {"date": None, "city" : None}
+        users_d[message.from_user.id] = {"date": None, "city": None}
 
-    bot.reply_to(message, "Привет!, данный бот поможет вам узнать расписание фильмов сети Киногалактика\n\nДля начала работы введи город из списка:\n/Чебоксары\n/Одинцово\n/Лыткарино")
+    bot.reply_to(message,
+                 "Привет!, данный бот поможет вам узнать расписание фильмов сети Киногалактика\n\nДля начала работы выбери город из списка:\n/cheboksari\n/odintsovo\n/lytkarino")
 
 
 # Ввод города
-@bot.message_handler(commands=["Чебоксары","Одинцово","Лыткарино"])
+@bot.message_handler(commands=["cheboksari", "odintsovo", "lytkarino"])
 def check_city(message):
-    city_dict = {
-        "/Чебоксары" : "cheboksari",
-        "/Одинцово" : "odintsovo",
-        "/Лыткарино" : "lytkarino", 
-    }
-    print("message.text",message.text)
-    city = city_dict[message.text]
-    users_d[message.from_user.id]["city"] = city
+    if message.from_user.id in users_d:
+        city_dict = {
+            "/cheboksari": "cheboksari",
+            "/odintsovo": "odintsovo",
+            "/lytkarino": "lytkarino",
+        }
+        rus_city_dict = {
+            "/cheboksari": "Чебоксары",
+            "/odintsovo": "Одинцово",
+            "/lytkarino": "Лыткарино",
+        }
 
-    bot.reply_to(message, "Введите дату в формате 12.12.2019 для получения расписания сеансов")
+        city = city_dict[message.text]
+        users_d[message.from_user.id]["city"] = city
+
+        bot.reply_to(message, "Выбранный вами город - " + rus_city_dict[
+            message.text] + "\nВведите дату в формате 12.12.2019 для получения расписания сеансов")
+
 
 # Ввод даты
 @bot.message_handler(func=lambda message: True)
 def check_date(message):
-    if message.from_user.id in users_d and users_d[message.from_user.id]["city"] != None :
+    if message.from_user.id in users_d and users_d[message.from_user.id]["city"] != None:
         current_city = users_d[message.from_user.id]["city"]
-        print("current_city",current_city)
         current_date = message.text
         date = current_date.split(".")
-        new_date = date[2]+"/"+date[1]+"/"+date[0]
+        new_date = date[2] + "/" + date[1] + "/" + date[0]
         users_d[message.from_user.id]["date"] = new_date
         bot.reply_to(message, "Подождите..")
-        #Работа парсера + там же отдаются сообщения пользователю
+        # Работа парсера + там же отдаются сообщения пользователю
         parser.html_parser(bot, message, new_date, current_city)
-       
+
+
 bot.polling()
