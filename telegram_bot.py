76089ec282bee77a80ceb6d0610b1119e9542744
DmitryTrollgiev <58760467+DmitryTrollgiev@users.noreply.github.com>
2019-12-12 10:45:45 +0300

    Разобрались с ботом и ответами

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,13 +8,14 @@ bot = telebot.TeleBot(API_TOKEN)
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+    bot.reply_to(message, "Здравствуйте, данный бот поможет вам узнать расписание фильмов двух киносетей:\n /1 - Расписание сети Киногалактика\n /2 - Расписание сети киноквартал")
 
-@bot.message_handler(commands=['kot'])
-def send_check(message):
-    bot.reply_to(message, "РАБОТАЕТ")
+@bot.message_handler(commands=['1'])
+def first_cinema_site(message):
+    bot.reply_to(message, "Киногалактика")
+
+@bot.message_handler(commands=['2'])
+def secind_cinema_site(message):
+    bot.reply_to(message, "Киноквартал")
 
 bot.polling()
