3e2752cc8b4d77dd01fe7f472263ef74b876389d
DmitryTrollgiev <58760467+DmitryTrollgiev@users.noreply.github.com>
2019-12-12 15:23:16 +0300

    Вынесли в отдельную функцию парсинг отдельной страницы фильма

--- parser.py
+++ parser.py
@@ -6,18 +6,58 @@ def get_site(url):
     r = requests.get(url).text
     return r
 
+def locale_html_parser(url):
+
+    locale_dict = {}
+    #Парсим ссылку с фильмом
+    locale_soup = BeautifulSoup(get_site(url), "lxml")
+    all_timetables = locale_soup.find("div", class_="sc-bdVaJa sc-bwzfXH iNSiJW schedule")
+                
+    #Время всех сеансов текущего фильма
+    time_list = []
+    for time in all_timetables.find_all("div", class_="sc-bdVaJa sc-bwzfXH buSbSY"):
+        time_list.append(time.text)
+                
+    #Цена в руб для всех сеансов текущего фильма
+    price_list = []
+    for price in all_timetables.find_all("div", class_="sc-bdVaJa sc-bwzfXH sw9zb-0 dDMLGB price"):
+        price_list.append(price.text)
+                
+    #Тип фильма (3D/2D) для всех сенсов текущего фильма
+    film_type_list = []
+    for film_type in all_timetables.find_all("div", class_="sc-bdVaJa sc-bwzfXH gYCEQ sw9zb-1 jXcRgy formats"):
+        film_str = film_type.text
+        if film_str == "":
+            film_type_list.append("2D")
+        elif film_str == "3D":
+            film_type_list.append(film_str)
+        else:
+            film_type_list.append("???")
+                    
+
+    print(time_list)
+    print(price_list)
+    print(film_type_list)
+
+    if len(time_list) == len(price_list) and len(film_type_list) == len(time_list):
+        #Заносим в словарь
+        for i in range(len(time_list)):
+            locale_dict[time_list[i]] = {"price": price_list[i], "type": film_type_list[i]}
+        return locale_dict
+    else:
+        return {}
+    
 def html_parser(date, city):
 
     URL = "https://kino-galaktika.ru"
-    #Одинцово
-    #sc-1h5tg87-0 duYTXo title
-    
 
+    #Основной словарь 
     films_dict = {"films": {}, "exception": False}
-    #r = get_site("https://kino-galaktika.ru/cinema/?date="+date+"&city="+city+"&facility=kinogalaktika")
     r = get_site(URL+"/?date="+date+"&city="+city)
     soup = BeautifulSoup(r, "lxml")
     
+    #Словарь для хранения фильма и ссылки на него
+    films_urls = {}
     #Формируем Список всех фильмов
     for link in soup.find_all("h2", class_="sc-1h5tg87-0 duYTXo title"):
         #Находим ссылку на фильм
@@ -26,46 +66,18 @@ def html_parser(date, city):
                 
                 film_name = link.text
                 if film_name not in films_dict:
-                    films_dict["films"][film_name] = {}
+                    films_urls[film_name] = URL+a['href']
+                
+    
+    #Для каждого фильма получаем расписание в отельной функции
+    print(films_urls)
+    
+    for film in films_urls:
+        result = locale_html_parser(films_urls[film])
+        films_dict["films"][film] = result
+        break
 
-                print(film_name)
-                print(a['href'])
 
-                #Парсим ссылку с фильмом
-                locale_soup = BeautifulSoup(get_site(URL+a['href']), "lxml")
-                all_timetables = locale_soup.find("div", class_="sc-bdVaJa sc-bwzfXH iNSiJW schedule")
-                
-                #Время всех сеансов текущего фильма
-                time_list = []
-                for time in all_timetables.find_all("div", class_="sc-bdVaJa sc-bwzfXH buSbSY"):
-                    time_list.append(time.text)
-                
-                #Цена в руб для всех сеансов текущего фильма
-                price_list = []
-                for price in all_timetables.find_all("div", class_="sc-bdVaJa sc-bwzfXH sw9zb-0 dDMLGB price"):
-                    price_list.append(price.text)
-                
-                #Тип фильма (3D/2D) для всех сенсов текущего фильма
-                film_type_list = []
-                for film_type in all_timetables.find_all("div", class_="sc-bdVaJa sc-bwzfXH gYCEQ sw9zb-1 jXcRgy formats"):
-                    film_str = film_type.text
-                    if film_str == "":
-                        film_type_list.append("2D")
-                    elif film_str == "3D":
-                        film_type_list.append(film_str)
-                    else:
-                        film_type_list.append("???")
-                    
-                #########
-                print(time_list)
-                print(price_list)
-                print(film_type_list)
-                if len(time_list) == len(price_list) and len(film_type_list) == len(time_list):
-                    #Заносим в словарь
-                    for i in range(len(time_list)):
-                        films_dict["films"][film_name][time_list[i]] = {"price": price_list[i], "type": film_type_list[i]}
-                else:
-                    films_dict["exception"] = True
 
     return films_dict
 
